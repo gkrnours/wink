58f34a091fcf192073abe052d69715362772f9d1
ludovic coues <gkr@jellycopter.net>
2018-12-06 20:58:27 +0100

    Add some kind of menu

--- src/main/js/menu.js
+++ src/main/js/menu.js
@@ -4,3 +4,23 @@ function start() {
 	var menu = document.querySelector("#menu ul");
 	menu.firstElementChild.classList.add("active");
 }
+
+function next() {
+	var current = document.querySelector("#menu ul .active");
+	var next = current.nextElementSibling;
+	if (!next) {
+		next = document.querySelector("#menu ul").firstElementChild;
+	}
+	current.classList.remove("active");
+	next.classList.add("active");
+}
+
+function prev() {
+	var current = document.querySelector("#menu ul .active");
+	var prev = current.previousElementSibling;
+	if (!prev) {
+		prev = document.querySelector("#menu ul").lastElementChild;
+	}
+	current.classList.remove("active");
+	prev.classList.add("active");
+}
